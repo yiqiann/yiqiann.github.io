4238511389f56827f49cc76b91bc6fa5e7cff9ed
avril <acha0154@student.monash.edu>
2023-10-16 03:52:42 +0800

    Small changes (graph wouldn't appear)

--- 3179-Assignment2/vega_lite_vis.js
+++ 3179-Assignment2/vega_lite_vis.js
@@ -1,4 +1,4 @@
 var spec1 = "https://raw.githubusercontent.com/yiqiann/yiqiann.github.io/main/3179-Assignment2/vis/avgpercentage_line_chart.json"
-vegaEmbed('#line_grapj', spec1).then(function(result) {
+vegaEmbed('#linegraph', spec1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
