8cbd1bbab4d92b0c969b7d98eafcebca8208e978
avril <acha0154@student.monash.edu>
2023-10-16 04:06:45 +0800

    Updated index.html and js file with visualisation
    Bug: graphs/charts still not appearing

--- 3179-Assignment2/vega_lite_vis.js
+++ 3179-Assignment2/vega_lite_vis.js
@@ -2,3 +2,13 @@ var spec1 = "https://raw.githubusercontent.com/yiqiann/yiqiann.github.io/main/31
 vegaEmbed('#linegraph', spec1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var spec2 = "https://raw.githubusercontent.com/yiqiann/yiqiann.github.io/main/3179-Assignment2/vis/stacked_bar_chart.json"
+vegaEmbed('#stackedbarchart', spec2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var spec2 = "https://raw.githubusercontent.com/yiqiann/yiqiann.github.io/main/3179-Assignment2/vis/mhd_chloropleth_map.json"
+vegaEmbed('#chloroplethmap', spec2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
